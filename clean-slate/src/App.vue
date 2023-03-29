7eb9eeda5a1d8f4b4957a025b79b4fc972b5518f
Dan <dmalengela@yahoo.com>
2023-03-29 08:44:55 +0200

    Created about, services and contact pages

--- clean-slate/src/App.vue
+++ clean-slate/src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <HomePage/>
+  <AboutPage/>
+  <ServicesPage/>
+  <ContactPage/>
 </template>
 
 <script>
 import HomePage from './components/pages/HomePage.vue'
+import AboutPage from './components/pages/AboutPage.vue'
+import ContactPage from './components/pages/ContactPage.vue'
+import ServicesPage from './components/pages/ServicesPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HomePage
+    HomePage, AboutPage, ServicesPage, ContactPage
   }
 }
 </script>
---
+++ clean-slate/src/components/pages/ServicesPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+    name: 'ServicesPage',
+    }
+</script>
+
+
+<style scoped>
+</style>
