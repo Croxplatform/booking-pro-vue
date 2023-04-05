9166a4dfbbc4014e5320e7b6a2290c43c7084e9f
Dan <dmalengela@yahoo.com>
2023-04-05 14:36:50 +0200

    Edited Contact view page structure

--- clean-slate/src/views/ContactPage.vue
+++ clean-slate/src/views/ContactPage.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Page Header Start -->
-    <div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
+    <div class="container-fluid page-header mb-5 p-0" style="background-image: url(/Assets/img/carousel-1.jpg);">
             <div class="container-fluid page-header-inner py-5">
                 <div class="container text-center pb-5">
                     <h1 class="display-3 text-white mb-3 animated slideInDown">Contact</h1>
@@ -15,52 +15,6 @@
             </div>
     </div>
     <!-- Page Header End -->
-
-
-    <!-- Booking Start -->
-    <div class="container-fluid booking pb-5 wow fadeIn" data-wow-delay="0.1s">
-            <div class="container">
-                <div class="bg-white shadow" style="padding: 35px;">
-                    <div class="row g-2">
-                        <div class="col-md-10">
-                            <div class="row g-2">
-                                <div class="col-md-3">
-                                    <div class="date" id="date1" data-target-input="nearest">
-                                        <input type="text" class="form-control datetimepicker-input"
-                                            placeholder="Check in" data-target="#date1" data-toggle="datetimepicker" />
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="date" id="date2" data-target-input="nearest">
-                                        <input type="text" class="form-control datetimepicker-input" placeholder="Check out" data-target="#date2" data-toggle="datetimepicker"/>
-                                    </div>
-                                </div>
-                                <div class="col-md-3">
-                                    <select class="form-select">
-                                        <option selected>Adult</option>
-                                        <option value="1">Adult 1</option>
-                                        <option value="2">Adult 2</option>
-                                        <option value="3">Adult 3</option>
-                                    </select>
-                                </div>
-                                <div class="col-md-3">
-                                    <select class="form-select">
-                                        <option selected>Child</option>
-                                        <option value="1">Child 1</option>
-                                        <option value="2">Child 2</option>
-                                        <option value="3">Child 3</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <button class="btn btn-primary w-100">Submit</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </div>
-    <!-- Booking End -->
 
 
     <!-- Contact Start -->
@@ -132,12 +86,24 @@
             </div>
     </div>
     <!-- Contact End -->
+
+
+    <!-- Newsletter -->
+    <Newsletter />
+    <!-- Newsletter -->
+
+    <br><br><br><br><br><br><br><br>
     
 </template>
 
 <script>
+    import Newsletter from '@/components/Modules/Newsletter.vue';
+
     export default {
-    name: 'ContactPage',
+        name: 'ContactPage',
+        components: {
+            Newsletter
+        }
     }
 </script>
 
